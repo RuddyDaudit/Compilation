5e2e1bfec6f49c1bc7c5a32eb6fda02652c5a725
RuddyDaudit <ruddydaudit@gmail.com>
2022-04-29 00:48:09 +0200

    imput

--- main.ml
+++ main.ml
@@ -1,15 +1,13 @@
 (*fichier main.ml *)
 
 let _ =
-
 try
-	let lexbuf = Lexing.from_channel stdin in
-	while true do
+	let file  =  open_in Sys.argv.(1) in
+	let lexbuf = Lexing.from_channel file in
 		Parseur.main Lexeur.token lexbuf
-		|> Printf.printf "%i\n%i";
-	done
 with
 | Lexeur.Eof -> exit 0
-| Lexeur.TokenInconnu
+| Lexeur.TokenInconu -> 
+	Printf.printf ("lex error \n")
 | Parsing.Parse_error ->
-	Printf.printf ("Ceci n'est pas une expression arithmetique ")
+	Printf.printf ("Ceci n'est pas une expression arithmetique \n")
